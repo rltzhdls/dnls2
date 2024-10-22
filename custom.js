03e0764e60f6120b9a2c489a6692344fa5e4e2c5
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-22 14:28:24 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineage/update/240124/main_loop.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
